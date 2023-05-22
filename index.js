7c7db252b31cf798206bc8c31bc471585a13e64a
PearlPerfect <zenithyonex4life@gmail.com>
2023-05-22 23:34:26 +0100

    okay

---
+++ index.js
@@ -0,0 +1,42 @@
+var toggleButtons = document.getElementsByClassName("button");
+var arr = [...toggleButtons];
+let screenDisplay = document.getElementById('screen');
+
+arr.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      element.style.opacity = "1";
+      if (index == 0) {
+        document.documentElement.setAttribute('data-theme', 'default');
+      } else if (index == 1) {
+        document.documentElement.setAttribute('data-theme', 'Orange');
+      } else {
+        document.documentElement.setAttribute('data-theme', "Blue");
+      }
+   
+      arr
+        .filter(function (item) {
+          return item != element;
+        })
+        .forEach((item) => {
+          item.style.opacity = "0";
+        });
+    });
+  });
+
+  function appendValue (value){
+    screenDisplay.value += value;
+  }
+
+  function Cls(){
+    screenDisplay.value = " ";
+  }
+
+  function Del() {
+    screenDisplay.value = screenDisplay.value.slice(0, -1)
+  }
+
+  function Answer(){
+    let equation = screenDisplay.value
+    let result = eval(equation)
+    screenDisplay.value = result
+  }
